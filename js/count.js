5b21fabfe4882bb74eb86d8dce157faa3633d0d8
Genya10 <113167825+Genya10@users.noreply.github.com>
2023-03-14 13:55:08 +0200

    Add files via upload

---
+++ js/count.js
@@ -0,0 +1,109 @@
+'use strict'
+//////////////////////////////////////////
+const inp1One = document.querySelector('.inp1');
+const inp1Two = document.querySelector('.inp2');
+const inp1Three = document.querySelector('.inp3');
+const sum1 = document.querySelector('.sum1');
+const btnSum1 = document.querySelector('.btn-sum1');
+btnSum1.addEventListener('click', function () {
+  let sumInp = inp1One.value * inp1Two.value * inp1Three.value;
+  sum1.textContent = sumInp;
+})
+
+const inp1Four = document.querySelector('.inp4');
+const inp1Five = document.querySelector('.inp5');
+const inp1Six = document.querySelector('.inp6');
+const sum2 = document.querySelector('.sum2');
+const btnSum2 = document.querySelector('.btn-sum2');
+btnSum2.addEventListener('click', function () {
+  let sumInp = inp1Four.value * inp1Five.value * inp1Six.value;
+  sum2.textContent = sumInp;
+})
+//////////////////////////////////////////////
+const inp2One = document.querySelector('.inp7');
+const inp2Two = document.querySelector('.inp8');
+const inp2Three = document.querySelector('.inp9');
+const sum3 = document.querySelector('.sum3');
+const btnSum3 = document.querySelector('.btn-sum3');
+btnSum3.addEventListener('click', function () {
+  let sumInp = inp2One.value * inp2Two.value * inp2Three.value;
+  sum3.textContent = sumInp;
+})
+
+const inp2Four = document.querySelector('.inp10');
+const inp2Five = document.querySelector('.inp11');
+const inp2Six = document.querySelector('.inp12');
+const sum4 = document.querySelector('.sum4');
+const btnSum4 = document.querySelector('.btn-sum4');
+btnSum4.addEventListener('click', function () {
+  let sumInp = inp2Four.value * inp2Five.value * inp2Six.value;
+  sum4.textContent = sumInp;
+});
+/////////////////////////////////////////////////
+const inp3One = document.querySelector('.inp13');
+const inp3Two = document.querySelector('.inp14');
+const inp3Three = document.querySelector('.inp15');
+const sum5 = document.querySelector('.sum5');
+const btnSum5 = document.querySelector('.btn-sum5');
+btnSum5.addEventListener('click', function () {
+  let sumInp = inp3One.value * inp3Two.value * inp3Three.value;
+  sum5.textContent = sumInp;
+})
+
+const inp3Four = document.querySelector('.inp16');
+const inp3Five = document.querySelector('.inp17');
+const inp3Six = document.querySelector('.inp18');
+const sum6 = document.querySelector('.sum6');
+const btnSum6 = document.querySelector('.btn-sum6');
+btnSum6.addEventListener('click', function () {
+  let sumInp = inp3Four.value * inp3Five.value * inp3Six.value;
+  sum6.textContent = sumInp;
+});
+///////////////////////////////////////////////////
+const inp4One = document.querySelector('.inp19');
+const inp4Two = document.querySelector('.inp20');
+const inp4Three = document.querySelector('.inp21');
+const sum7 = document.querySelector('.sum7');
+const btnSum7 = document.querySelector('.btn-sum7');
+btnSum7.addEventListener('click', function () {
+  let sumInp = inp4One.value * inp4Two.value * inp4Three.value;
+  sum7.textContent = sumInp;
+})
+
+const inp4Four = document.querySelector('.inp22');
+const inp4Five = document.querySelector('.inp23');
+const inp4Six = document.querySelector('.inp24');
+const sum8 = document.querySelector('.sum8');
+const btnSum8 = document.querySelector('.btn-sum8');
+btnSum8.addEventListener('click', function () {
+  let sumInp = inp4Four.value * inp4Five.value * inp4Six.value;
+  sum8.textContent = sumInp;
+});
+///////////////////////////////////////////////
+const inp5One = document.querySelector('.inp25');
+const inp5Two = document.querySelector('.inp26');
+const inp5Three = document.querySelector('.inp27');
+const sum9 = document.querySelector('.sum9');
+const btnSum9 = document.querySelector('.btn-sum9');
+btnSum9.addEventListener('click', function () {
+  let sumInp = inp5One.value * inp5Two.value * inp5Three.value;
+  sum9.textContent = sumInp;
+})
+
+const inp5Four = document.querySelector('.inp28');
+const inp5Five = document.querySelector('.inp29');
+const inp5Six = document.querySelector('.inp30');
+const sum10 = document.querySelector('.sum10');
+const btnSum10 = document.querySelector('.btn-sum10');
+btnSum10.addEventListener('click', function () {
+  let sumInp = inp5Four.value * inp5Five.value * inp5Six.value;
+  sum10.textContent = sumInp;
+});
+
+/*const totalKg1 = document.querySelector('span-kg');
+let total1 = sum1 + sum2;
+const btnKg1 = document.querySelector('.total_kg>button');
+console.log(btnKg1);
+btnKg1.addEventListener('click', function () {
+  totalKg1.innerHTML = '55';
+});*/
---
+++ js/total.js
@@ -0,0 +1,58 @@
+'use strict'
+const totalKg = document.querySelector('.span-kg');
+const totalNumb = document.querySelector('.span-numb');
+const average = document.querySelector('.span-average');
+
+const btnKg1 = document.querySelector('.btn-total1');
+const btnKg2 = document.querySelector('.btn-total2');
+const btnKg3 = document.querySelector('.btn-total3');
+
+
+let sumAll = document.querySelectorAll('.sum');
+console.log(sumAll);
+let sum = 0;
+
+btnKg1.addEventListener('click', function () {
+  for (let i = 0; i < sumAll.length; i++) {
+    let sumNumb = sumAll[i].value;
+    let numb = Number(sumNumb.textContent);
+    sum += numb;
+  }
+  totalKg.textContent = sum;
+});
+
+
+/*let numbAll = document.querySelectorAll('.numb');
+console.log(numbAll);
+let number = 0;
+btnKg2.addEventListener('click', function () {
+  for (let i = 0; i < numbAll.length; i++) {
+    let suNumb = numbAll[i];
+    console.log(suNumb);
+    let numb1 = Number(suNumb.textContent);
+    console.log(numb1);
+    number += numb1;
+  }
+  totalNumb.textContent = number;
+});*/
+
+btnKg2.addEventListener('click', function () {
+  let sumNumb = (inp1Two.value * inp1Three.value)
+    + (inp1Five.value * inp1Six.value)
+    + (inp2Two.value * inp2Three.value)
+    + (inp2Five.value * inp2Six.value)
+    + (inp3Two.value * inp3Three.value)
+    + (inp3Five.value * inp3Six.value)
+    + (inp4Two.value * inp4Three.value)
+    + (inp4Five.value * inp4Six.value)
+    + (inp5Two.value * inp5Three.value)
+    + (inp5Five.value * inp5Six.value)
+  totalNumb.textContent = sumNumb;
+});
+
+btnKg3.addEventListener('click', function () {
+  let aver = totalKg.textContent / totalNumb.textContent;
+  let averMath = aver.toFixed(2);
+  average.textContent = averMath;
+});
+
